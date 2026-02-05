4e16695674ee34ab5254f959b0c233b3b7b399d1
bobo-SOC <140917342+Nikolov999@users.noreply.github.com>
2026-02-05 13:48:20 +0000

    Delete Projects/backend directory

--- Projects/backend/app/services/diff.py
+++
@@ -1,8 +0,0 @@
-def diff_scans(old: dict, new: dict):
-    added_ports = list(set(new.get("ports", [])) - set(old.get("ports", [])))
-    removed_ports = list(set(old.get("ports", [])) - set(new.get("ports", [])))
-
-    return {
-        "added_ports": added_ports,
-        "removed_ports": removed_ports
-    }
